42e023841aac61202a0dfa632578d4b109247518
uvantino <yuvrajsingh.c@antino.com>
2024-05-16 15:57:00 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+       <p
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    ></p>
+     <body className={`${inter.className} antialiased`}>{children}</body>
+     
     </html>
   );
 }
